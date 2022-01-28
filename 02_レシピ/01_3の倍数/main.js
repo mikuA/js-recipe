dbb01cb94b8d0854d1e9c1e25c19e0a05c6fdb67
mikuA <akmt920@gmail.com>
2022-01-29 02:52:08 +0900

    元気な秒針

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,3 +1,20 @@
+//発展1
+const fizzbuzz = function (number) {
+  for (let n = 0; n <= number; n++) {
+    if (n % 15 === 0) {
+      console.log(`FizzBuzz`)
+    } else if (n % 3 === 0) {
+      console.log(`Fizz`)
+    } else if (n % 5 === 0) {
+      console.log(`Buzz`)
+    } else {
+      console.log(n)
+    }
+  }
+}
+
+//fizzbuzz(100)
+//発展2
 const genkiFunction = function (number) {
   for (let n = 1; n <= number; n++) {
     if (n % 3 === 0 || /3/.test(n)) {
@@ -8,4 +25,30 @@ const genkiFunction = function (number) {
   }
 }
 
-genkiFunction(100)
+//genkiFunction(100)
+
+//発展3
+
+let count = 0
+let maxValue = 0
+
+const a = function (number) {
+  maxValue = number
+  //setIntervalの引数の関数は引数を持つことができない
+  setInterval(countUp, 1000)
+}
+
+const countUp = function () {
+  count += 1
+  if (count > maxValue) {
+    //setIntervalを止める
+    clearInterval(a)
+  } else {
+    count += 1
+    if (count % 3 === 0 || /3/.test(count)) {
+      console.log(`${count}!!!!!!!`)
+    } else {
+      console.log(count)
+    }
+  }
+}
